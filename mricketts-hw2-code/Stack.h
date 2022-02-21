410c5575c5370086614808ab47503fe6cb310bed
matti3r <matti3r@protonmail.com>
2022-02-20 22:22:39 -0600

    First commit

---
+++ mricketts-hw2-code/Stack.h
@@ -0,0 +1,26 @@
+#pragma once
+template <typename Object>
+class Stack
+{
+public:
+    bool isEmpty() const
+    {
+        return theList.size() == 0;
+    }
+    const Object& top() const
+    {
+        return theList.front();
+    }
+    void push(const Object& x)
+    {
+        theList.push_front(x);
+    }
+    Object& pop()
+    {
+        Object retVal = theList.front();
+        theList.remove(theList.begin());
+        return retVal;
+    }
+private:
+    List<Object> theList;
+};
